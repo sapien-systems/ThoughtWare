2684eaaa5d7d7f9cdb555e2925d180e75244fcc3
mkbeefcake <mkblockchaindev@outlook.com>
2022-12-01 01:08:04 +0800

    initial version

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from "react";
+import { Box, Button, VStack } from "@react-native-material/core";
+import { ScrollView } from "react-native";
+
+export default function App() {
+  return (
+    <>
+      <ScrollView style={{backgroundColor:"black"}}>
+        <VStack>
+          <Box h={100} style={{ backgroundColor: "#faf089" }}>
+
+          </Box>
+        </VStack>
+      </ScrollView>
+    </>
+  )
+}
+ 
